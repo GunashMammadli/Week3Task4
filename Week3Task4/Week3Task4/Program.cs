ea16f67318cd70e34457e302e30b6cb147b36eba
GunashMammadli <146819479+GunashMammadli@users.noreply.github.com>
2023-10-19 22:45:42 +0400

    Done: Week3Task4

---
+++ Week3Task4/Week3Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Week3Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User Gunash = new User("Gunash", "sun45945", "gunash03@gmail.com");
+
+
+            
+
+        }
+    }
+}
---
+++ Week3Task4/Week3Task4/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Task4
+{
+    internal class User
+    {
+        public string Username;
+        public string Password;
+        public string Email;
+        public bool LoggedIn;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            LoggedIn = false;
+        }
+
+        public User(string username, string password, string email) : this(username, password) 
+        {
+            Email = email;
+        }
+
+
+
+        static void Login(bool LoggedIn)
+        {
+            if (LoggedIn == false) 
+            { 
+                Console.WriteLine("Istifadəçi hesaba daxil oldu");
+            }
+            else 
+            {
+                Console.WriteLine("Istifadəçi öncədən hesaba daxil olub");
+            }
+        }
+        static void Logout(bool LoggedIn)
+        {
+            if (LoggedIn == true)
+            {
+                Console.WriteLine("Istifadəçi hesabdan çıxış etdi ");
+            }
+            else
+            {
+                Console.WriteLine("Istifadəçi hesaba giriş etməyib");
+            }
+        }
+    }
+}
